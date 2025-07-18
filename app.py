2d49eda80b97d6876eec8a96355b636d4efc4dc6
mchena <m.chena@itecriocuarto.org.ar>
2025-07-17 22:28:38 -0300

    flask login en app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+    current_user,
+    LoginManager,
+    login_required,
+    login_user,
+    logout_user,
+)
 
-
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
 
 
 app = Flask(__name__)
@@ -12,11 +22,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     'mysql+pymysql://root:@localhost/miniblog'
 )
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
-
 
 
 
