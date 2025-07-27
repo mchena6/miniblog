e50939bdc1b27da5a7055f1c0745409a24fcc65c
mchena <m.chena@itecriocuarto.org.ar>
2025-07-27 18:35:07 -0300

    Rutas para crear un post, ver posts y perfil de usuario creadas

--- app.py
+++ app.py
@@ -31,11 +31,67 @@ migrate = Migrate(app, db)
 
 from models import User, Post, Comment
 
+# Relaciones de la base de datos:
+# user.posts -> post de un usuario
+# user.comments -> comentarios de un usuario
+# post.comments -> comentarios de un post
+# post.author -> usuario que posteo
+# comment.author -> usuario que escribio un comentario
+# comment.post -> post al que pertenece un comentario
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
+@app.route('/login')
+def login():
+    return render_template(
+        'auth/login.html'
+    )
+
+@app.route('/register')
+def register():
+    return render_template(
+        'auth/register.html'
+    )
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+# Ruta principal
 @app.route('/')
 def index():
+    posts = Post.query.order_by(Post.created_at.desc()).all()
     return render_template(
-        'index.html'
+       'index.html', posts=posts
+    )
+
+# Ver un post especifico
+@app.route('/post/<int:id>')
+def post_detail():
+    post = Post.query.get_or_404(id)
+    return render_template(
+        'post_detail.html', post=post
+    )
+
+# Perfil de un usuario
+@app.route('/user/<int:id>')
+def user_profile():
+    user = User.query.get_or_404(id)
+    return render_template(
+        'user_profile.html', user=user
+    )
+
+# Todos los posteos de un usuario
+@app.route('/user/<int:id>/posts')
+def user_posts():
+    user = User.query.get_or_404(id)
+    posts = Post.query.filter_by(id).order_by(Post.created_at.desc()).all()
+    return render_template(
+        'user_posts.html', user=user, posts=posts
     )
 
